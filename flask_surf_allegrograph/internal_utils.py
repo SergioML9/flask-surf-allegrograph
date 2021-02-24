06f55b927c88ee7a1e9f5874a556738cf3d9cae1
Sergio Muñoz <sergiomunlop@gmail.com>
2021-02-24 16:12:35 +0100

    back to the source

--- flask_surf_allegrograph/internal_utils.py
+++ flask_surf_allegrograph/internal_utils.py
@@ -1,10 +1,9 @@
 from flask import current_app
 
 def get_allegrograph_session():
-    with current_app.app_context():
-        try:
-            return current_app.extensions["surf-allegrograph"]["session"]
-        except KeyError:
-            raise RuntimeError(
-                "You must initiliaze an Allegrograph session"
-            )
+    try:
+        return current_app.extensions["surf-allegrograph"]["session"]
+    except KeyError:
+        raise RuntimeError(
+            "You must initiliaze an Allegrograph session"
+        )
