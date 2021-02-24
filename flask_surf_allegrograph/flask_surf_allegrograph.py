369e540bfc1ed94946c9b08f3eb1a7b1dc7932df
Sergio Muñoz <sergiomunlop@gmail.com>
2021-02-24 13:35:32 +0100

    Initial commit

---
+++ flask_surf_allegrograph/flask_surf_allegrograph.py
@@ -0,0 +1,39 @@
+import surf
+from flask import current_app, _app_ctx_stack
+
+
+class SurfAllegrograph(object):
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.config.setdefault('AGRAPH_HOST', 'localhost')
+        app.config.setdefault('AGRAPH_PORT', '10035')
+        app.teardown_appcontext(self.teardown)
+
+    def connect(self):
+        store = surf.Store(reader = 'allegro_franz', 
+                   writer = 'allegro_franz',
+                   server = current_app.config['AGRAPH_HOST'],
+                   port = current_app.config['AGRAPH_PORT'],
+                   catalog = 'ewetasker',
+                   repository = 'ewetasker-db')
+
+        print('Create the session')
+        session = surf.Session(store,{})
+        return session
+
+    def teardown(self, exception):
+        ctx = _app_ctx_stack.top
+        if hasattr(ctx, 'allegrograph_db'):
+            ctx.allegrograph_db.close()
+
+    @property
+    def connection(self):
+        ctx = _app_ctx_stack.top
+        if ctx is not None:
+            if not hasattr(ctx, 'allegrograph_db'):
+                ctx.allegrograph_db = self.connect()
+            return ctx.allegrograph_db
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+setup(
+    name='Flask-SuRF-Allegrograph',
+    version='0.1',
+    url='http://github.com/SergioML9/flask-surf-allegrograph/',
+    license='BSD',
+    author='Sergio Muñoz',
+    author_email='sergio.munoz@upm.es',
+    description='Very short description',
+    long_description=__doc__,
+    packages=['flask_surf_allegrograph'],
+    # if you would be using a package instead use packages instead
+    # of py_modules:
+    # packages=['flask_sqlite3'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'surfrdf @ git+ssh://git@github.com:franzlst/surfrdf.git',
+        'surfrdf-allegrofranz-plugin @ git+ssh://git@github.com:SergioML9/surfrdf-allegrofranz-plugin.git',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
